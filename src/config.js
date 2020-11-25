5a75397d4d3eef2ff97cfada684d1704bfbe16bc
AJBates2 <aj.bates2.92@gmail.com>
2020-11-24 22:05:31 -0600

    only one port option

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    PORT: process.env.PORT || 8000,
+    PORT: process.env.PORT,
     NODE_ENV: process.env.NODE_ENV || 'development',
     DATABASE_URL: process.env.DATABASE_URL || 'postgresql://master:1@localhost/SafeRes',
     TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://master:1@localhost/SafeRes-test',
